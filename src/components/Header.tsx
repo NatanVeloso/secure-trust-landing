7a0b7b3f322ce5ed1016f56f268884c457fc9004
Natan Veloso <dev.natanveloso@gmail.com>
2025-12-29 19:05:42 -0300

    ajuste em import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
 import logo from "@/assets/logo.png";
 import ThemeToggle from "./ThemeToggle";
 import { Phone, Menu, X } from "lucide-react";
