afd2abcbfeb1d461d0b32cb6a73f1b06d82b8a56
Natan Veloso <dev.natanveloso@gmail.com>
2025-12-30 01:36:32 -0300

    ajustes parceiros

--- src/components/PartnersCarousel.tsx
+++ src/components/PartnersCarousel.tsx
@@ -1,31 +1,26 @@
 import { useState } from "react";
-
-// Import das logos dos parceiros
-import allianz from "@/assets/parceiros/Allianz.png";
-import allseg from "@/assets/parceiros/allseg.png";
-import axxa from "@/assets/parceiros/axxa.png";
-import azos from "@/assets/parceiros/azos.png";
-import azul from "@/assets/parceiros/azul.png";
-import bb from "@/assets/parceiros/bb.png";
-import bradesco from "@/assets/parceiros/bradesco.png";
-import darwin from "@/assets/parceiros/darwin.png";
-import ezzeSeguros from "@/assets/parceiros/Ezze-seguros.png";
-import hdiSeguros from "@/assets/parceiros/hdi-seguros-logo.png";
-import itau from "@/assets/parceiros/itau.png";
-import justos from "@/assets/parceiros/justos.png";
-import mapfre from "@/assets/parceiros/mapfre.png";
-import mitsui from "@/assets/parceiros/mitsui.png";
-import pier from "@/assets/parceiros/pier.png";
-import porto from "@/assets/parceiros/porto.png";
-import sancor from "@/assets/parceiros/sancor.png";
-import suhai from "@/assets/parceiros/suhai.png";
-import tokioMarine from "@/assets/parceiros/tokio-marine.png";
-import zurich from "@/assets/parceiros/zurich.png";
+import bb from "../assets/parceiros/bp.png";
+import axxa from "../assets/parceiros/axxa.png";
+import azos from "../assets/parceiros/azos.png";
+import azul from "../assets/parceiros/azul.png";
+import itau from "../assets/parceiros/itau.png";
+import pier from "../assets/parceiros/pier.png";
+import porto from "../assets/parceiros/porto.png";
+import suhai from "../assets/parceiros/suhai.png";
+import justos from "../assets/parceiros/justos.png";
+import mapfre from "../assets/parceiros/mapfre.png";
+import mitsui from "../assets/parceiros/mitsui.png";
+import sancor from "../assets/parceiros/sancor.png";
+import allseg from "../assets/parceiros/allseg.png";
+import darwin from "../assets/parceiros/darwin.png";
+import zurich from "../assets/parceiros/zurich.png";
+import allianz from "../assets/parceiros/Allianz.png";
+import bradesco from "../assets/parceiros/bradesco.png";
+import ezzeSeguros from "../assets/parceiros/Ezze-seguros.png";
+import tokioMarine from "../assets/parceiros/tokio-marine.png";
+import hdiSeguros from "../assets/parceiros/hdi-seguros-logo.png";
 
 const PartnersCarousel = () => {
-  const [isPaused, setIsPaused] = useState(false);
-
-  // Lista de logos dos parceiros
   const partners = [
     { name: "Allianz", logo: allianz },
     { name: "Allseg", logo: allseg },
@@ -64,23 +59,14 @@ const PartnersCarousel = () => {
           </p>
         </div>
 
-        <div
-          className="relative"
-          onMouseEnter={() => setIsPaused(true)}
-          onMouseLeave={() => setIsPaused(false)}
-        >
+        <div className="relative">
           {/* Gradientes nas bordas para efeito de fade */}
           <div className="absolute left-0 top-0 bottom-0 w-32 bg-gradient-to-r from-background to-transparent z-10 pointer-events-none" />
           <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-l from-background to-transparent z-10 pointer-events-none" />
 
           {/* Container do carrossel */}
           <div className="flex overflow-hidden">
-            <div
-              className={`flex gap-8 ${isPaused ? 'animation-paused' : ''}`}
-              style={{
-                animation: 'scroll 40s linear infinite',
-              }}
-            >
+            <div className="flex gap-8 animate-scroll">
               {duplicatedPartners.map((partner, index) => (
                 <div
                   key={`${partner.name}-${index}`}
@@ -89,8 +75,7 @@ const PartnersCarousel = () => {
                   <img
                     src={partner.logo}
                     alt={partner.name}
-                    className="max-w-full max-h-full object-contain filter grayscale hover:grayscale-0 transition-all duration-300"
-                  />
+                    className="max-w-full max-h-full object-contain" />
                 </div>
               ))}
             </div>
@@ -108,8 +93,8 @@ const PartnersCarousel = () => {
           }
         }
 
-        .animation-paused {
-          animation-play-state: paused !important;
+        .animate-scroll {
+          animation: scroll 90s linear infinite;
         }
       `}</style>
     </section>
